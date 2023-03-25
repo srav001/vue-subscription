7dabbd7a35e85304aa62425dbfa6cadd8c7068da
srav001 <sravansuresh1x@gmail.com>
2023-03-25 21:18:00 +0530

    fix: build process

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,31 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
+import { resolve } from 'node:path';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+	build: {
+		minify: 'esbuild',
+		lib: {
+			entry: resolve(__dirname, 'src/index.ts'),
+			name: 'vue-subscription',
+			fileName: 'vue-subscription'
+		},
+		rollupOptions: {
+			external: ['vue'],
+			output: {
+				globals: {
+					vue: 'Vue'
+				}
+			}
+		}
+	},
+	plugins: [vue(), dts()],
+	resolve: {
+		alias: {
+			'@': fileURLToPath(new URL('./src', import.meta.url))
+		}
+	}
+});
