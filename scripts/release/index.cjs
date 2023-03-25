dc6de8bd63d1d0d147b70b86f8036a3d1e97f472
srav001 <sravansuresh1x@gmail.com>
2023-03-25 23:23:31 +0530

    chore: releasing version 0.0.4

--- scripts/release/index.cjs
+++ scripts/release/index.cjs
@@ -6,10 +6,13 @@ async function gitUpdate() {
 		const releaseData = JSON.parse(fs.readFileSync(releaseFile, { encoding: 'utf8' }));
 		const version = releaseData.version;
 
+		releaseData.onGoing = false;
+		fs.writeFileSync(releaseFile, JSON.stringify(releaseData, null, 2));
+
 		execWithSync(`git add .`);
 		execWithSync(`git commit -m "chore: releasing version ${version}"`);
 		execWithSync(`git tag ${version}`);
-		// execWithSync(`git push origin ${version}`);
+		execWithSync(`git push origin ${version}`);
 
 		return true;
 	} catch (e) {
@@ -19,14 +22,10 @@ async function gitUpdate() {
 
 async function npmRelease() {
 	try {
-		const releaseFile = path.resolve('scripts/release/releaseData.json');
-		const releaseData = JSON.parse(fs.readFileSync(releaseFile, { encoding: 'utf8' }));
-
 		execWithSync(`npm version`);
 		execWithSync(`npm publish`);
 
-		releaseData.onGoing = false;
-		return fs.writeFileSync(releaseFile, JSON.stringify(releaseData, null, 2));
+		return true;
 	} catch (e) {
 		console.error(e);
 	}
